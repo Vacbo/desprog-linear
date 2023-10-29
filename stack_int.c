1a3e3a12b6ec6f834aadbd5f039a38418385b403
Pedro Balbo Portella <53411412+Vacbo@users.noreply.github.com>
2023-10-29 18:49:58 -0300

    Update stack_int.c

--- stack_int.c
+++ stack_int.c
@@ -49,3 +49,32 @@ int stack_int_pop(stack_int *s) {
     free(n);
     return value;
 }
+
+// Aula 14a ex2
+int stack_int_genpop(stack_int *s, int index) {
+    // Get size
+    int size = 0;
+    for (node *n = s->top; n != NULL; n = n->next) {
+        size++;
+    }
+
+    // Get node to remove and get value
+    node *n = s->top;
+    node *prev = NULL;
+    for (int i = 0; i < size - index; i++) {
+        prev = n;
+        n = n->next;
+    }
+
+    // If node to remove is the first one, update top
+    if (prev == NULL) {
+        s->top = n->next;
+    } else { // Else, update previous node next to skip the node to remove
+        prev->next = n->next;
+    }
+
+    int value = n->value; 
+    free(n); // Free node
+
+    return value;
+}
